86a04b0af03ec68d708ee75d0421d1f589d9cad4
Jacopo Groppo <94755689+getJack@users.noreply.github.com>
2021-11-22 19:15:45 +0000

    Setup di Repl

---
+++ somma.py
@@ -0,0 +1,9 @@
+lista_numeri = [3, 3, 5, 1, 2]
+
+def somma(lista_numeri):
+    risultato = 0
+    for item in lista_numeri:
+        risultato = risultato + item
+    printf('risultao: {}'.format(risultao))
+
+    risultato = somma(lista_numeri)
